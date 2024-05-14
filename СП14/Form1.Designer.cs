116f90e0292c9c678a228de8d22c9e9a1ad09b3e
Coffey <H1.coffey@bk.ru>
2024-05-15 01:33:48 +0700

    14

---
+++ СП14/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace СП14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(10, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(262, 438);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "0");
+            this.imageList1.Images.SetKeyName(1, "1");
+            this.imageList1.Images.SetKeyName(2, "2");
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(296, 69);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(774, 381);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(291, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "DRIVE:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 522);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ СП14/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace СП14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoaadDrives();
+        }
+        private void LoaadDrives()
+        {
+            try
+            {
+                DriveInfo[] drives = DriveInfo.GetDrives();
+
+                foreach (var drive in drives)
+                {
+                    if (drive.IsReady)
+                    {
+                        TreeNode treeNode = treeView1.Nodes.Add(drive.Name);
+                        treeNode.Tag = drive.RootDirectory.FullName;
+                        treeNode.ImageIndex = 0;
+                        treeNode.Nodes.Add("");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("При загрузке дисков произошла ошибка:" + ex.Message);
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            try
+            {
+                TreeNode selectedNode = e.Node;
+                selectedNode.Nodes.Clear();
+                string path = (string)selectedNode.Tag;
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                foreach (var dir in directoryInfo.GetDirectories())
+                {
+                    TreeNode node = selectedNode.Nodes.Add(dir.Name);
+                    node.Tag = dir.FullName;
+                    node.ImageIndex = 1;
+                    node.Nodes.Add("");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("При развертывании узла произошла ошибка: " + ex.Message);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            try
+            {
+                listView1.Items.Clear();
+                string path = (string)e.Node.Tag;
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                foreach (var file in directoryInfo.GetFiles())
+                {
+                    ListViewItem item = new ListViewItem(file.Name);
+                    item.SubItems.Add(file.Length.ToString());
+                    item.SubItems.Add(file.LastWriteTime.ToString());
+                    item.ImageIndex = 2;
+                    listView1.Items.Add(item);
+                }
+                DriveInfo driveInfo = new DriveInfo(Path.GetPathRoot(path));
+                label1.Text = string.Format("Drive: {0}", path);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("При выборе узла произошла ошибка: " + ex.Message);
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems.Count > 0)
+                {
+                    string path = Path.Combine(treeView1.SelectedNode.Tag.ToString(), listView1.SelectedItems[0].Text);
+                    if (File.Exists(path))
+                    {
+                        System.Diagnostics.Process.Start(path);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Выбранный элемент не является файлом.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("При открытии файла произошла ошибка: " + ex.Message);
+            }
+        }
+    }
+}
